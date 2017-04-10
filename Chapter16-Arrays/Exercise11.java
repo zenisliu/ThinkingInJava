b747b9a03c49f019f54437c714825837cbb5c59e
zenis <zenisliu@gmail.com>
2017-04-11 01:05:22 +0800

    Chapter 16, Exercise 11

---
+++ Chapter16-Arrays/Exercise11.java
@@ -0,0 +1,12 @@
+package com.zenisliu.chapter16;
+
+/**
+ * Created by zenisliu on 17/4/11.
+ * Show that autoboxing doesn't work with arrays
+ */
+public class Exercise11 {
+    public static void main(String[] args) {
+        // Integer[] ia = new int[10]; // Incompatible error!!
+        // int[] i = new Integer[10];  // Incompatible error!!
+    }
+}
